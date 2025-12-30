1a260290b275519f2414b2456e0692ad089b027e
KoyoNoguchi <koo.ooy504405@gmail.com>
2025-12-31 01:08:06 +0900

    weekページを大改築した． 授業変更に対応した．

--- routes/routes.js
+++ routes/routes.js
@@ -5,9 +5,11 @@ const controller = require('../controllers/controller');
 router.get('/', controller.renderHomePage);
 router.get('/year', controller.renderYearPage);
 router.get('/week', controller.renderWeekPage);
-router.get('/subjects/:code', controller.renderSubjectPage);
+router.get('/api/week_list', controller.getWeekScheduleData);
+router.get('/api/subject/:code', controller.getSubjectData);
+router.get('/subject/:code', controller.renderSubjectPage);
 router.get('/test_schedule', controller.renderSchedulePage);
-router.get('/library', controller.renderLibraryPage);
-router.get('/api/year', controller.redirectToYearPage);
+router.get('/library', controller.redirectToLibraryPage);
+router.get('/api/year', controller.redirectToYearSchedulePage);
 
 module.exports = router;
